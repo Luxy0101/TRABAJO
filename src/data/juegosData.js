808e2473b6c6b83a9e4f2fdb3458435633712c1d
luxyxd <luxywe1oxd@gmail.com>
2022-11-07 14:13:50 -0500

    luxy

--- src/data/juegosData.js
+++ src/data/juegosData.js
@@ -2,61 +2,78 @@ import callofduty from '../images/callofduty.jpg'
 import apex from '../images/apex.jpg'
 import rocket from '../images/rocket.jpg'
 import team from '../images/team.jpg'
+import leagueoflegends from "../images/lol.png"
+import gtav from "../images/gtav.jpg"
+import gtaIV from "../images/gtaiv.jpg"
+import narutostorm4 from "../images/naruto.jpg"
+import jumpforce from "../images/jump.jpg"
+import amongus from "../images/sus.jpg"
+import fallguys from "../images/fallguys.jpg"
+import vicecity from "../images/tommy.jpg"
+import cyberpunk2077 from "../images/punk.jpg"
+import fifa13 from "../images/elfifas.jpg"
+import battlefield1 from "../images/batle2.jpg"
+import southpark from "../images/sout.jpg"
+import undertale from "../images/underruan.jpg"
+import godofowar from "../images/godof.jpg"
+
+
+
 
 export const juegosData = [
   {
-    top: 0, gratis: false, oferta: true, image: callofduty, id: '1', nombre: 'Call of Duty', newPrice: 200, porcentaje: 45, oldPrice: 5500, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: false, oferta: true, image: callofduty, id: '1', nombre: 'Call of Duty', newPrice: 200, porcentaje: 45, oldPrice: 5500, descripcion: 'Call of Duty: Modern Warfare es un videojuego de disparos en primera persona desarrollado por Infinity Ward y publicado por Activision.'
   },
   {
-    top: 0, gratis: false, oferta: true, image: apex, id: '2', nombre: 'Apex Legends', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: false, oferta: true, image: apex, id: '2', nombre: 'Apex Legends', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Apex Legends es un videojuego gratuito perteneciente a los géneros battle royale y hero shooter en primera persona, desarrollado por Respawn Entertainment y publicado por Electronic Arts.'
   },
   {
-    top: 0, gratis: false, oferta: true, image: rocket, id: '3', nombre: 'Rocket League', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: false, oferta: true, image: rocket, id: '3', nombre: 'Rocket League', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: ' Rocket League es un videojuego que combina el fútbol con los vehículos. Fue desarrollado por Psyonix y lanzado el 7 de julio de 2015'
   },
   {
-    top: 0, gratis: false, oferta: true, image: team, id: '4', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: false, oferta: true, image: battlefield1, id: '4', nombre: 'Battlefield 1', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Battlefield 1 te lleva hasta la Primera Guerra Mundial, donde nueva tecnología y un conflicto a nivel mundial cambiaron la guerra para siempre. Participa en cada batalla, controla cada enorme vehículo y ejecuta cada maniobra que transforma por completo un combate.'
   },
   {
-    top: 0, gratis: true, oferta: false, image: team, id: '5', nombre: 'Team Fortress 2', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: true, oferta: false, image: leagueoflegends, id: '5', nombre: 'League of legends', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'League of Legends, es un videojuego multijugador de arena de batalla en línea desarrollado y publicado por Riot Games.'
   },
   {
-    top: 0, gratis: true, oferta: false, image: team, id: '6', nombre: 'Team Fortress 2', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: true, oferta: false, image: fifa13, id: '6', nombre: 'Fifa 13', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Powered by Football™. EA SPORTS™ FIFA 22 acerca aún más el juego a la realidad gracias a mejoras significativas en la jugabilidad y una nueva temporada de novedades en todos los modos.'
   },
   {
-    top: 0, gratis: true, oferta: false, image: team, id: '7', nombre: 'Team Fortress 2', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: true, oferta: false, image: gtaIV, id: '7', nombre: 'Gta IV', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Niko intenta dejar atrás el pasado y forjarse una nueva vida en la tierra de las oportunidades. Johnny, un veterano de la banda de moteros Lost, se encuentra atrapado en una salvaje guerra territorial entre bandas rivales que desean controlar la ciudad. Luis, mitad criminal, mitad ayudante a tiempo completo del legendario empresario de locales nocturnos Tony Prince (alias "Gay Tony"), lucha para competir por la lealtad de familiares y amigos en un mundo en el que todos tienen un precio. Sus vidas se cruzarán con consecuencias devastadoras mientras luchan por sobrevivir en una ciudad destrozada por la violencia y la corrupción.'
   },
   {
-    top: 0, gratis: true, oferta: false, image: team, id: '8', nombre: 'Team Fortress 2', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: true, oferta: false, image: narutostorm4, id: '8', nombre: 'Naruto storm 4', newPrice: null, porcentaje: null, oldPrice: null, descripcion: '¡Con el nuevo título de la aclamada saga STORM emprenderás un viaje impresionante y lleno de color! ¡Benefíciate de un sistema de combate totalmente renovado y prepárate para sumergirte en los combates más épicos que hayas visto nunca!'
   },
   {
-    top: 0, gratis: true, oferta: false, image: team, id: '9', nombre: 'Team Fortress 2', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: true, oferta: false, image: gtav, id: '9', nombre: 'Gta V', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el galardonado mundo de Los Santos y el condado de Blaine con una resolución de 4K y disfrutar del juego a 60 fotogramas por segundo.'
   },
   {
-    top: 0, gratis: true, oferta: false, image: team, id: '10', nombre: 'Team Fortress 2', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: true, oferta: false, image: jumpforce, id: '10', nombre: 'Jump Force', newPrice: null, porcentaje: null, oldPrice: null, descripcion: 'Crea tu propio avatar y lucha junto a los héroes más fuertes del manga en el original modo Historia o entra en la sala online para desafiar a otros jugadores y descubrir multitud de modos y actividades.'
   },
   {
-    top: 1, gratis: false, oferta: false, image: team, id: '11', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 1, gratis: false, oferta: false, image: amongus, id: '11', nombre: 'Among Us', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Un juego de socialización local o en línea de trabajo en equipo y traición para 4 a 15 jugadores... ¡ambientado en el espacio!'
   },
   {
-    top: 1, gratis: false, oferta: false, image: team, id: '12', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 1, gratis: false, oferta: false, image: fallguys, id: '12', nombre: 'Fallguys', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Fall Guys es un videojuego de plataformas y battle royale desarrollado por Mediatonic y publicado inicialmente por Devolver Digital y más tarde por Epic Games.'
   },
   {
-    top: 1, gratis: false, oferta: false, image: team, id: '13', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 1, gratis: false, oferta: false, image: vicecity, id: '13', nombre: 'Vice City', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Grand Theft Auto: Vice City, es un videojuego de acción-aventura de mundo abierto en tercera persona. Es el quinto título de la serie Grand Theft Auto y el segundo en gráficos 3D. '
   },
   {
-    top: 1, gratis: false, oferta: false, image: team, id: '14', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 1, gratis: false, oferta: false, image: cyberpunk2077, id: '14', nombre: 'CyberPunk 2077', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Cyberpunk 2077 es un RPG de aventura y acción de mundo abierto ambientado en el futuro sombrío de Night City, una peligrosa megalópolis obsesionada con el poder, el glamur y las incesantes modificaciones corporales.'
   },
   {
-    top: 1, gratis: false, oferta: false, image: team, id: '15', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 1, gratis: false, oferta: false, image: southpark, id: '15', nombre: 'South Park the stick of truth', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'South Park: The Stick of Truth es un videojuego de rol basado en la serie de televisión animada estadounidense South Park. '
   },
   {
-    top: 1, gratis: false, oferta: false, image: team, id: '16', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 1, gratis: false, oferta: false, image: team, id: '16', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: null, oldPrice: null, descripcion: 'Team Fortress 2, abreviado por su acrónimo TF2, es un videojuego multijugador de disparos en primera persona desarrollado y publicado por Valve Corporation.'
   },
   {
-    top: 0, gratis: false, oferta: true, image: team, id: '17', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: false, oferta: true, image: undertale, id: '17', nombre: 'Undertale', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Undertale es un videojuego de rol en 2D de 2015 creado por el desarrollador independiente estadounidense Toby Fox. El jugador controla a un niño que ha caído al subsuelo: una gran región aislada bajo la superficie de la Tierra, separada por una barrera mágica. El jugador se encuentra con varios monstruos durante su viaje de regreso a la superficie, aunque algunos monstruos pueden involucrar al jugador en una pelea.'
   },
   {
-    top: 0, gratis: false, oferta: true, image: team, id: '18', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    top: 0, gratis: false, oferta: true, image: godofowar, id: '18', nombre: 'God of War 2', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'God of War es un videojuego de acción-aventura desarrollado por SCE Santa Monica Studio y publicado por Sony Interactive Entertainment'
   },
 
 ]
@@ -71,6 +88,6 @@ export const juegosSlider = [
     gratis: false, oferta: false, image: rocket, id: '3', nombre: 'Rocket League', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
   },
   {
-    gratis: false, oferta: true, image: team, id: '4', nombre: 'Team Fortress 2', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Es un juego de accion de tercera persona que se basa en la 2da guerra mundial'
+    gratis: false, oferta: true, image: undertale, id: '4', nombre: 'Undertale', newPrice: 200, porcentaje: 45, oldPrice: 400, descripcion: 'Undertale es un videojuego de rol en 2D de 2015 creado por el desarrollador independiente estadounidense Toby Fox. El jugador controla a un niño que ha caído al subsuelo: una gran región aislada bajo la superficie de la Tierra, separada por una barrera mágica.' 
   },
 ]
